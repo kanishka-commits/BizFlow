12a26963de0c762d84686b3d57471595c9c1319c
nallarahul <nallarahu515@gmail.com>
2025-08-20 23:24:41 +0530

    Fixed the UI of Pricing Section

--- src/components/PricingSection.jsx
+++ src/components/PricingSection.jsx
@@ -9,7 +9,7 @@ const PricingCard = ({ name, price, features, animation }) => (
     variants={animation}
     className="rounded-lg bg-gradient-to-r from-pink-200 to-blue-100 p-8 shadow-lg"
   >
-    <h3 className="text-xl  text-gray-900 mb-4">{name}</h3>
+    <h3 className="text-xl  text-black mb-4">{name}</h3>
     <p className="text-3xl text-black font-bold mb-6">${price}/mo</p>
     <ul className="list-disc list-inside pl-5 text-gray-700 space-y-2 mb-6">
       {features.map((feat) => (
@@ -109,7 +109,7 @@ const PricingSection = () => {
               variants={fadeIn('up', 1.3)}
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
-              className="bg-pink-200 text-gray-900 px-6 py-3 rounded-lg transition duration-300 hover:bg-pink-300 hover:shadow-lg cursor-pointer"
+              className="bg-pink-200 text-black px-6 py-3 rounded-lg transition duration-300 hover:bg-pink-300 hover:shadow-lg cursor-pointer"
               onClick={handleStartButton}
           >
               Get Started
